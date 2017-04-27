4898a857348e5d367ec48df5c9060d04bed9cb85
sebastien@sifnos.io <sebastien@sifnos.io>
2017-04-28 01:09:09 +0200

    allow unconfirmed access_for 1 day

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
-          :omniauthable
-          # :confirmable
+          :omniauthable #, :confirmable
   include DeviseTokenAuth::Concerns::User
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,4 +2,5 @@ Devise.setup do |config|
   # Using rails-api, tell devise to not use ActionDispatch::Flash
   # middleware b/c rails-api does not include it.
   config.navigational_formats = [:html, :json]
+  config.allow_unconfirmed_access_for = 7.days
 end
