e5b72d9a9d3350a656e4af628e28c4caf0267a50
sebastien@sifnos.io <sebastien@sifnos.io>
2017-05-04 00:27:25 +0200

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": "airbnb",
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        'react/jsx-filename-extension': 'off',
+        'no-console': [
+          'error',
+          {
+            allow: ['warn', 'error', 'info'],
+          },
+        ]        
+    }
+};
